ce255f9f94e3e4b32ee005432a4675758bead2f6
Dylan Miller <dmanmiller2004@gmail.com>
2024-11-09 14:25:09 -0500

    Implement URL and model settings in ProgramController

--- programcontroller.cpp
+++ programcontroller.cpp
@@ -3,3 +3,35 @@
 ProgramController::ProgramController(QObject *parent)
     : QObject{parent}
 {}
+
+/*
+    Sets the URL of the Ollama server.
+*/
+void ProgramController::setURL(QString url)
+{
+    ollama.setURL(url.toStdString());
+}
+
+/*
+    Returns the URL of the Ollama server.
+*/
+QString ProgramController::getURL() const
+{
+    return QString::fromStdString(ollama.getURL());
+}
+
+/*
+    Sets the model to use.
+*/
+void ProgramController::setModel(QString model)
+{
+    ollama.setModel(model.toStdString());
+}
+
+/*
+    Returns the model to use.
+*/
+QString ProgramController::getModel() const
+{
+    return QString::fromStdString(ollama.getModel());
+}
--- programcontroller.h
+++ programcontroller.h
@@ -2,6 +2,9 @@
 #define PROGRAMCONTROLLER_H
 
 #include <QObject>
+#include <QString>
+#include <QtQmlIntegration>
+#include "ollamainterface.h"
 
 /*
     Serves as the interface to C++ from QML.
@@ -9,9 +12,33 @@
 class ProgramController : public QObject
 {
     Q_OBJECT
+    QML_ELEMENT
+
 public:
     explicit ProgramController(QObject *parent = nullptr);
 
+    /*
+        Sets the URL of the Ollama server.
+    */
+    Q_INVOKABLE void setURL(QString url);
+
+    /*
+        Returns the URL of the Ollama server.
+    */
+    Q_INVOKABLE QString getURL() const;
+
+    /*
+        Sets the model to use.
+    */
+    Q_INVOKABLE void setModel(QString model);
+
+    /*
+        Returns the model to use.
+    */
+    Q_INVOKABLE QString getModel() const;
+
+private:
+    OllamaInterface ollama;
 };
 
 #endif // PROGRAMCONTROLLER_H
